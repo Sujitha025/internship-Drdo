20b77c69df1795b23de105de297942e4cc093239
Sujitha025 <106320322+Sujitha025@users.noreply.github.com>
2024-02-20 11:47:34 +0530

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,10 @@
+export default {
+    JWT_SECRET : "6pmDNvUnUwuZxqT42v2geo5QuZJeVOXzSqia5Q+YQ8Q=",
+    EMAIL:"lela.robel@ethereal.email",
+    PASSWORD: "ek2RpPcXPdyfAeh55M",
+    ATLAS_URI:"mongodb+srv://admin:admin123@cluster0.r4flvw8.mongodb.net/",
+    SERVER_DOMAIN: "http://localhost:8080/",
+    SERVICE_ID: 'service_z5bfeuo',
+    TEMPLATE_ID: 'template_r46nmws',
+    PUBLIC_KEY: 'SBBIgV0tIZPhf79hk',
+}
